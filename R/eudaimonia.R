5687aeaeacd648e718c3f4970044e1de96cb90f4
medewitt <me.dewitt.jr@gmail.com>
2018-10-31 17:01:09 -0400

    Added EI plus some structural stuff like coc and license

---
+++ R/eudaimonia.R
@@ -0,0 +1,120 @@
+#' Memor PDF Memo Template
+#'
+#' @param use_profile T/F value for whether the user profile in
+#' `~/memor-profile.yaml` will be loaded. Default is `TRUE`.
+#' @param logo File path to Company logo file. The logo will be printed on
+#' the top right corner of the first page and the size will be adjusted to
+#' `logo_height`.
+#' @param company Company Name as the page footnote. You can input a list of
+#' values, such as company name, address, telephone and email.
+#' @param short_title A shorten version of the title to be printed on the
+#' top-right corner of every page, except the front page.
+#' @param watermark Watermark Text to be printed on the page. You can change
+#' its color via `watermark_color`.
+#' @param confidential T/F value for whether a red confidential sign will
+#' be printed.
+#' @param libertine T/F. Libertine is a collection of open fonts for western
+#' languages. We found it fits this template quite well. Default is `FALSE`
+#' though we highly recommend you to turn it on.
+#' @param chinese T/F for Chinese language support. Default is `FALSE`.
+#' @param logo_height Height of the logo image. This logo image will be scaled
+#' to height and the default height is 1.2cm.
+#' @param watermark_color Color for the watermark text. Default is "gray".
+#' @param footer_on_first_page T/F value for whether the company info footer
+#' will be displayed on the front page.
+#' @param toc T/F value for table of contents. See ?rmarkdown::pdf_document
+#' for details
+#' @param lot T/F value for list of tables.
+#' @param lof T/F value for list of figures.
+#' @param number_sections T/F value for whether sections should be numbered.
+#' See ?rmarkdown::pdf_document for details
+#' @param fancy_captions T/F value for whether to apply special formatting to
+#' captions.
+#' @param latex_engine LaTeX engine. See ?rmarkdown::pdf_document for details
+#' @param ... other options to be passed to rmarkdown::pdf_document. See
+#' ?rmarkdown::pdf_document for details
+#'
+#' @importFrom rmarkdown pdf_document pandoc_variable_arg
+#' @import knitr
+#'
+#' @examples
+#' \dontrun{
+#' library(rmarkdown)
+#'  example <- system.file("example/demo.Rmd", package = "memor")
+#'  render(example, memor::pdf_memo())
+#' }
+#'
+#' @export
+eudaimonia <- function(
+                     logo = NULL, company = NULL, short_title = NULL,
+                     watermark = NULL, confidential = FALSE,
+                     libertine = FALSE, chinese = FALSE,
+                     logo_height = "1.2cm", watermark_color = "gray",
+                     footer_on_first_page = TRUE,
+                     toc = FALSE, lot = FALSE, lof = FALSE,
+                     fancy_captions = TRUE,
+                     number_sections = TRUE, latex_engine = "xelatex", ...) {
+
+  memor_args <- c()
+
+  if (!is.null(logo)) {
+    memor_args <- c(memor_args, pandoc_variable_arg("logo", logo),
+                    pandoc_variable_arg("logo_height", logo_height))
+  }
+
+  if (!is.null(company)) {
+    if (is.list(company) && length(company) > 1) {
+      company <- paste(unlist(company),
+                       collapse = " \\hspace{.025 in} $\\cdot$ \\hspace{.05 in} ")
+    }
+    memor_args <- c(memor_args, pandoc_variable_arg("company", company))
+  }
+
+  if (!is.null(short_title)) {
+    memor_args <- c(memor_args, pandoc_variable_arg("short_title", short_title))
+  }
+
+  if (footer_on_first_page) {
+    memor_args <- c(memor_args,
+                    pandoc_variable_arg("footer_on_first_page", "yes"))
+  }
+
+  if (confidential) {
+    memor_args <- c(memor_args, pandoc_variable_arg("confidential", "yes"))
+  }
+
+  if (libertine) {
+    memor_args <- c(memor_args, pandoc_variable_arg("libertine", "yes"))
+  }
+
+  if (chinese) {
+    memor_args <- c(memor_args, pandoc_variable_arg("chinese", "yes"))
+  }
+
+  if (!is.null(watermark)) {
+    memor_args <- c(memor_args, pandoc_variable_arg("watermark", watermark),
+                    pandoc_variable_arg("watermark_color", watermark_color))
+  }
+
+  if (lot) memor_args <- c(memor_args, pandoc_variable_arg("lot", "yes"))
+  if (lof) memor_args <- c(memor_args, pandoc_variable_arg("lof", "yes"))
+
+  if (fancy_captions) {
+    memor_args <- c(memor_args, pandoc_variable_arg("fancy_captions", "yes"))
+  }
+
+  template <- system.file("rmarkdown/templates/eudaimonia/resources/eudaimonia.tex",
+                          package = "wfutemplates")
+  config <- rmarkdown::pdf_document(
+    latex_engine = latex_engine,
+    template = template,
+    number_sections = number_sections,
+    toc = toc,
+    ...
+  )
+
+  pre_pandoc <- config$pandoc$args
+  config$pandoc$args <- c(pre_pandoc, memor_args)
+
+  return(config)
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# wfutemplates
+
+The goal of `wfutemplates` is to codify some commonly used templates at
+WFU and make report generation easier while adhering to a common visual
+identity. This package is largely inspired by the `memor`
+[package](https://github.com/hebrewseniorlife/memor) with slowly
+evolving tweaks.
+
+This package has a lot of the Wake Forest University Logos.
+
+## Installation
+
+You can install the released version of wfutemplates from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("wfutemplates")
+```
+
+# Contributing
+
+Please note that the ‘wfutemplates’ project is released with a
+[Contributor Code of Conduct](CODE_OF_CONDUCT.md). By contributing to
+this project, you agree to abide by its terms.
