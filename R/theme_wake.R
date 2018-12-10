4577b4c6e31848f7a9f12ab7aa4c748ca4d0b67f
Michael DeWitt <me.dewitt.jr@gmail.com>
2018-12-09 20:29:26 -0500

    add wake themes

---
+++ R/theme_wake.R
@@ -0,0 +1,23 @@
+#' Gggplot2 Theme for Wake Forest University
+#' @description This theme is a larger size for presentations consistent with
+#'     the WFU style guide
+#' @export
+
+theme_wake <- function(){
+  theme(panel.background = element_blank(),
+        plot.title = element_text(family = "Times"),
+        panel.grid = element_blank(),
+        legend.background = element_rect(fill="transparent", colour=NA),
+        legend.key = element_rect(fill="transparent", colour=NA),
+  )
+}
+
+#' @examples
+#'  ggplot(iris, aes(Sepal.Length,Sepal.Width, color = Species))+
+#'  geom_point()+
+#'  theme_wake()+
+#'  scale_color_wake()+
+#'  labs(
+#'    title = "The is the Wake Forest Title",
+#'    subtitle = "Yep"
+#'  )
